09fbf6e9d5bd81f415e023561e765874cba8d8d1
Md Kamran Taslim <54260472+KamranTaslim@users.noreply.github.com>
2023-05-11 23:03:23 +0530

    Add files via upload

---
+++ src/Dropdown.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Dropdown = () => {
+
+    const [isActive, setActive] = useState(false);
+    const [list] = useState(['Yes', 'Probably not']);
+    const [value, setValue] = useState('');
+
+    const onClickHandeler = (e) => {
+        setValue(e.target.textContent);
+        setActive(false);
+    }
+
+    const onMouseHover = () => {
+        setActive(true);
+    }
+
+    const results = list.map((result, index) => {
+        return (
+          <button onClick={onClickHandeler} key={index}>
+            {result}
+          </button>
+        );
+      });
+      
+    return (
+        <div className="dropdown">
+            <h1>Should you use a dropdown?</h1>
+            <h3>{value}</h3>
+            <button className="dropbtn" onMouseEnter={onMouseHover}>Select  
+            <img  src="https://cdn-icons-png.flaticon.com/128/2722/2722987.png" alt="dropdown arrow"/>
+            </button>
+            <div className="dropdown-content" style={{ display: `${isActive ? 'block' : 'none'}` }}>
+                {results}
+            </div>
+        </div>
+    );
+}
+
+export default Dropdown;
